f9682248a786dc9d65d919f9d8da8001a5b4a9d3
Егор <@DESKTOP-33RB6L8>
2022-10-11 23:17:14 +0300

    Separation DONE

---
+++ Print.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stdafx.h"
+#include "constsnts.h"
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void ReversePrint(int arr[], const int n);
